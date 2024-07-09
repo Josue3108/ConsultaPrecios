b99bfabbadac48585cf82b564124ecb895efcc35
Josue3108 <67967986+Josue3108@users.noreply.github.com>
2024-07-08 19:44:51 -0600

    Funcion de autoincrementar cantidad
    Se actualizo la funcion que autoincrementa la cantidad de veces que un producto entra al sistema

--- Prueba1.py
+++ Prueba1.py
@@ -1,18 +1,29 @@
 import pandas as pd
-import time as t
-
 
 file_path = 'PRECIOS_LAB_SJ1.xlsx'  # Replace with your actual file path
 df = pd.read_excel(file_path)
-prices =  pd.DataFrame()
-    
-#Search and save a item that has been purchased   
-def get_price_p(item):
-    if df.loc[df['NAME'] == item].isnull:
-        new_df = df.loc[(df['NAME'] == item)]
+prices = pd.DataFrame(columns=['NAME', 'PRICE', 'QUANTITY'])
+
+# Search and save an item that has been purchased
+def get_price_p(item, data):
+    prices = data
+    # Check if the item exists in the DataFrame and if there are any null values in the subset
+    if df.loc[df['NAME'] == item].isnull().any().any():
+        new_df = df.loc[df['NAME'] == item].copy()  # Use .copy() to avoid SettingWithCopyWarning
         new_df['QUANTITY'] = 1
         return new_df
     else:
-        print(prices)
+        if not prices.loc[prices['NAME'] == item].empty:
+            times = prices.loc[prices['NAME'] == item, 'QUANTITY'].iloc[0]
+            prices.loc[prices['NAME'] == item, 'QUANTITY'] = times + 1
+        else:
+            new_df = df.loc[df['NAME'] == item].copy()
+            new_df['QUANTITY'] = 1
+            prices = pd.concat([prices, new_df])
+        return prices
 
-prices = get_price_p('IgE ANTI BANANO')
+# Example usage
+prices = get_price_p('IgE ANTI BANANO',prices)
+prices = get_price_p('IgE ANTI BANANO',prices)
+prices = get_price_p('IgE ANTI BANANO',prices)
+print(prices)
